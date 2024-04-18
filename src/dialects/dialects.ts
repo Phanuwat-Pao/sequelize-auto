f7fb23950f41692a41c00627cecee3e6a8ab6aa2
phanuwat-pao <phanuwat.lua@gmail.com>
2024-04-18 09:03:29 +0700

    chore: Update .gitignore to exclude 'lib' directory and add 'types' directory

--- src/dialects/dialects.ts
+++ src/dialects/dialects.ts
@@ -1,14 +1,14 @@
-import { mssqlOptions } from "./mssql";
-import { mysqlOptions } from "./mysql";
-import { postgresOptions } from "./postgres";
-import { sqliteOptions } from "./sqlite";
-import { DialectOptions } from "./dialect-options";
-import { Dialect } from "sequelize";
+import { Dialect } from 'sequelize';
+import { DialectOptions } from './dialect-options';
+import { mssqlOptions } from './mssql';
+import { mysqlOptions } from './mysql';
+import { postgresOptions } from './postgres';
+import { sqliteOptions } from './sqlite';
 
 export const dialects: { [name in Dialect]: DialectOptions } = {
   mssql: mssqlOptions,
   mysql: mysqlOptions,
   mariadb: mysqlOptions,
   postgres: postgresOptions,
-  sqlite: sqliteOptions
+  sqlite: sqliteOptions,
 };
