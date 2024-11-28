cc41507683222d696587876fe5ed4e8d625e7528
phanuwat-pao <phanuwat.lua@gmail.com>
2024-11-28 11:47:17 +0700

    Enhance TypeScript declaration generation to support array types for optional fields

--- src/auto-generator.ts
+++ src/auto-generator.ts
@@ -741,7 +741,9 @@ export class AutoGenerator {
       if (!this.options.skipFields || !this.options.skipFields.includes(field)) {
         const name = this.quoteName(recase(this.options.caseProp, field));
         const isOptional = this.getTypeScriptFieldOptional(table, field);
-        str += `${sp}${name}?: ${this.getTypeScriptType(table, field)} ${isOptional ? '| null' : ''} | undefined;\n`;
+        str += `${sp}${name}?: ${this.getTypeScriptType(table, field)} ${
+          isOptional ? '| null' : ''
+        } | undefined | ${this.getTypeScriptType(table, field)}[];\n`;
       }
     });
     return str;
